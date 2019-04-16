e557d027bdb10672c87af20de867930c7b3f3f47
Matt Nguyen <matth3wn@gmail.com>
2019-04-16 07:16:43 -0700

    changed word list

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -22,30 +22,30 @@ INSERT INTO "language" ("id", "name", "user_id")
 VALUES
   (1, 'Spanish', 1);
 
-INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
-VALUES
-  (1, 1, 'palabra', 'word', 2),
-  (2, 1, 'semana', 'week', 3),
-  (3, 1, 'desafortunadamente', 'unfortunately', 4),
-  (4, 1, 'anaranjado', 'orange', 5),
-  (5, 1, 'quizá', 'maybe', 6),
-  (6, 1, 'no me digas', 'shut up', 7),
-  (7, 1, 'ayudame', 'help me', 8),
-  (8, 1, 'dónde está el baño', 'where is the bathroom', null);
-
---   INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+-- INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 -- VALUES
 --   (1, 1, 'palabra', 'word', 2),
 --   (2, 1, 'semana', 'week', 3),
 --   (3, 1, 'desafortunadamente', 'unfortunately', 4),
 --   (4, 1, 'anaranjado', 'orange', 5),
---   (5, 1, 'quizás', 'maybe', 6),
---   (6, 1, 'poco', 'small', 7),
---   (7, 1, 'perro', 'dog', 8),
---   (8, 1, 'gato', 'cat', 9),
---   (9, 1, 'segundo', 'second', 10),
---   (10, 1, 'reloj', 'clock', 11),
---   (11, 1, 'ayer', 'yesterday', null);
+--   (5, 1, 'quizá', 'maybe', 6),
+--   (6, 1, 'no me digas', 'shut up', 7),
+--   (7, 1, 'ayudame', 'help me', 8),
+--   (8, 1, 'dónde está el baño', 'where is the bathroom', null);
+
+  INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+VALUES
+  (1, 1, 'palabra', 'word', 2),
+  (2, 1, 'semana', 'week', 3),
+  (3, 1, 'desafortunadamente', 'unfortunately', 4),
+  (4, 1, 'anaranjado', 'orange', 5),
+  (5, 1, 'quizás', 'maybe', 6),
+  (6, 1, 'poco', 'small', 7),
+  (7, 1, 'perro', 'dog', 8),
+  (8, 1, 'gato', 'cat', 9),
+  (9, 1, 'segundo', 'second', 10),
+  (10, 1, 'reloj', 'clock', 11),
+  (11, 1, 'ayer', 'yesterday', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
